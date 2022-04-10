fc64cf5819981a0cbf8b775fae34b55d8ac7047e
KimtaiKogoKevin <49808720+KimtaiKogoKevin@users.noreply.github.com>
2022-04-10 23:23:28 +0300

    COntinue firebase setup min 40:59

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tryit_vendor_app/Screens/Authentication/loginscreen.dart';
 import 'package:tryit_vendor_app/Screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
